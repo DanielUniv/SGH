4b48d342619a1acc6618b474d031dbdacdbe5a3d
DanielUiv <daniel.gomez.suarez@correounivalle.edu.co>
2022-05-13 09:01:41 -0500

    Inicio conexion Backend

--- src/Backend/Server.java
+++ src/Backend/Server.java
@@ -1,8 +1,5 @@
 package Backend;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.net.ServerSocket;
 
 public class Server {
 
--- src/Frontend/Conexion.java
+++ src/Frontend/Conexion.java
@@ -1,31 +1,7 @@
 package Frontend;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
-public class Conexion extends JPanel implements ActionListener, KeyListener {
 
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+public class Conexion{
 
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
 }
