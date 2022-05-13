d8fc0e15fc33150a1c0ec77a5ed044114d9385d3
DanielUiv <daniel.gomez.suarez@correounivalle.edu.co>
2022-05-13 13:11:46 -0500

    conexion BD

--- src/Backend/DataBasesManager.java
+++ src/Backend/DataBasesManager.java
@@ -1,4 +1,45 @@
 package Backend;
 
+import javax.management.Query;
+import javax.swing.*;
+import java.sql.*;
+
 public class DataBasesManager {
+
+    private String ruta;
+    private String usuario;
+    private String pass;
+    private Connection net;
+    private PreparedStatement orden;
+
+    public  DataBasesManager(){
+        ruta = "jdbc:postgresql://localhost:5432/postgres";
+        usuario = "postgres";
+        pass = "basededatosprueba";
+
+        try{
+            net = DriverManager.getConnection(ruta,usuario,pass);
+            System.out.println("Base de De datos Conectada");
+        }catch (Exception ex){
+            System.out.println("Error al conectar: "+ ex.getMessage());
+        }
+
+    }
+
+    public void consultaQuery(String consulta,String[] datos){
+        try {
+            orden = net.prepareStatement(consulta);
+            for(int i = 0; i < datos.length;i++){
+                orden.setString((i + 1),datos[i]);
+            }
+            orden.executeQuery();
+        }catch (Exception e){
+
+        }
+    }
+
+    public String consultaEspecial(String query) throws SQLException {
+        orden = net.prepareStatement(query);
+    }
+
 }
--- src/Backend/Server.java
+++ src/Backend/Server.java
@@ -1,8 +1,15 @@
 package Backend;
 
 
+
 public class Server {
 
+    private DataBasesManager DB;
+    private String[] consultas;
+
+    public Server(){
+
+    }
 
 
 }
